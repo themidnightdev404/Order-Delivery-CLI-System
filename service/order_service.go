82a2552eaa6100d9f4b6e3adab2e0efc49beecf3
arS <themidnightdev404@gmail.com>
2026-07-16 21:41:22 +0300

    add order service constructor

--- service/order_service.go
+++ service/order_service.go
@@ -1,1 +1,18 @@
 package service
+
+import repository "order_system/repository"
+
+type OrderService struct {
+	customerRepo repository.CustomerRepository
+	orderRepo    repository.OrderRepository
+	discountCalc DiscountCalculator
+}
+
+func NewOrderService(customerRepo repository.CustomerRepository, orderRepo repository.OrderRepository, discountCalc DiscountCalculator) *OrderService {
+	return &OrderService{
+		customerRepo: customerRepo,
+		orderRepo:    orderRepo,
+		discountCalc: discountCalc,
+	}
+
+}
